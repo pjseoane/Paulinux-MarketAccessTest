19e67a233ff5a72e3cf9e088168bc68955c7f1d2
pjs <pjseoane@gmail.com>
2019-07-18 23:55:49 -0300

    1st commit intellij

---
+++ src/main/java/paulinux/MainAccess.java
@@ -0,0 +1,57 @@
+package paulinux;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+
+public class MainAccess{
+
+    public static String marketID;
+    public static String endpoint;
+    public static String usr;
+    public static String pswd;
+    public static String ticker;
+    public static String author;
+
+    public static void main(String[] args) throws Exception{
+
+
+            try (InputStream input = new FileInputStream ("src/main/java/configuration/config.properties")){
+
+                Properties prop =new Properties();
+                prop.load(input);
+
+                // *** upload variables in cfg file
+
+                marketID        =prop.getProperty("ra.marketID");
+                endpoint        =prop.getProperty("ra.endpoint");
+                usr             =prop.getProperty("ra.usr");
+                pswd            =prop.getProperty("ra.pswd");
+                ticker          =prop.getProperty("ra.exampleTicker");
+                author          =prop.getProperty("ra.author");
+
+
+                // loginREST newConnection = new loginREST();
+                System.out.println("Author :"+author);
+
+
+                // print outputs
+                System.out.println("\nREST Outputs:....");
+                System.out.println("\nTOKEN->>>>>>>>>>:"+loginREST.getToken());
+
+                String instruments =loginREST.getInstruments();
+                System.out.println("Elements "+instruments);
+
+                String instDetails =loginREST.getInstrumentDetail(ticker);
+                System.out.println("Details for "+ticker+" "+instDetails);
+
+
+            }catch (IOException ex){
+                System.out.println("Some error loading config.properties");
+            }
+
+        }
+}
---
+++ src/test/java/paulinux/MainAccessTest.java
@@ -0,0 +1,20 @@
+package paulinux;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MainAccess.
+ */
+public class MainAccessTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
